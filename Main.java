7a42b27f3e76e55ef872ac1af1a310e4b23f53d4
Kriva Anton <rezvaul12@gmail.com>
2016-10-27 21:29:16 +0300

    Fibonacci numbers

---
+++ Main.java
@@ -0,0 +1,39 @@
+package example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int parametr = Integer.parseInt(bf.readLine());
+
+		ArrayList<Integer> list = fibonacci(parametr);
+		for (int a : list) {
+			System.out.println(a);
+
+		}
+	}
+
+	public static ArrayList<Integer> fibonacci(int parametr) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		int sum;
+		int i = 0;
+		for (;;) {
+			sum = list.get(i) + list.get(i + 1);
+			i++;
+			if (sum <= parametr) {
+				list.add(sum);
+			} else
+				break;
+		}
+
+		return list;
+	}
+
+}
